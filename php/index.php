7023f58441dd956f7da8097b1e23f78c14683ba5
Veronica Celseti <veronicacelseti12@gmail.com>
2020-06-17 14:25:22 +0200

    stampa dei dischi in pagina con php, ciclo foreach per creare ogni disco

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<?php include 'database/lista_dischi.php' ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Dischi</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container-dischi">
+            <?php
+            foreach ($dischi as $disco) {?>
+                <div class="disco">
+                    <img src="<?php echo $disco['poster']?>" alt="">
+                    <h5 class="titolo"><?php echo $disco['title']?></h5>
+                    <p class="card-text"><?php echo $disco['author']?></p>
+                    <p class="card-text"><?php echo $disco['genre']?></p>
+                    <p class="card-text"><?php echo $disco['year']?></p>
+                    <?php
+            }?>
+
+
+
+
+
+
+            </div>
+
+        </div>
+    </body>
+</html>
